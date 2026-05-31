c8eaa2b61f03a4ca6666629b66346614937bfa72
RosaBen <benchabane.rosa@outlook.com>
2026-05-31 23:30:18 +0200

    added array with random numbers

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,26 @@
 import Die from "./components/Die"
 function App() {
 
+  function generateAllNewDice(){
+
+    // return new Array(10)
+    //         .fill(0)
+    //         .map(() => Math.ceil(Math.random() * 6))
+    const newDice = []
+
+    for (let i=0 ; i<10; i++){
+      const rand = Math.ceil(Math.random()*6)
+      newDice.push(rand)
+    }
+    return newDice
+  }
+
+  console.log(generateAllNewDice())
 
   return (
     <main>
       <section className="dice">
-              <Die value={1}/>
+      <Die value={1}/>
       <Die value={1}/>
       <Die value={2}/>
       <Die value={3}/>
