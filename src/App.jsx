dfe831e60963f2e11416bb03a73506560a798c62
RosaBen <benchabane.rosa@outlook.com>
2026-06-12 00:44:22 +0200

    added object in array with value, id, and isHeld boolean

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import { useState } from "react"
 import Die from "./components/Die"
+import {nanoid} from "nanoid"
 function App() {
 const [dice, setDice]=useState(generateAllNewDice())
   function generateAllNewDice(){
 
     // return new Array(10)
     //         .fill(0)
-    //         .map(() => Math.ceil(Math.random() * 6))
+    //         .map(() => ({value:Math.ceil(Math.random() * 6)), isHeld:false, id: nanoid()})
     const newDice = []
 
     for (let i=0 ; i<10; i++){
-      const rand = Math.ceil(Math.random()*6)
+      const rand = {value: Math.ceil(Math.random()*6), isHeld:false, id: nanoid()}
       newDice.push(rand)
     }
+
     return newDice
   }
 
-  const diceElements = dice.map((num)=> <Die value={num}/>)
+  
+
+  const diceElements = dice.map((num)=> <Die key={num.id} value={num.value}/>)
 
   function rollDice(){
     setDice(generateAllNewDice())
